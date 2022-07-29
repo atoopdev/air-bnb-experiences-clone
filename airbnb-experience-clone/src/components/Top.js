4b081a894ba958af9614bbd40e3ba874979de453
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-29 11:35:27 -0700

    hero html and css

--- airbnb-experience-clone/src/components/Top.js
+++ airbnb-experience-clone/src/components/Top.js
@@ -5,6 +5,9 @@ import photogrid from "../images/photo-grid.png"
 export default function Top(){
     return <header>
         <nav className="nav-bar"><img src={logo} className="logo-img" alt="airbnb logo"></img></nav>
+        <section className="hero">
         <img src={photogrid} className="hero-img" alt="photo collage"></img>
+        <h1 className="hero-header"> Online Experiences</h1>
+        <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p></section>
     </header>
 }
