c678d259819d066a6f9c6852f2a86ca0a93d8713
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-08-02 14:51:09 -0700

    experience card placeholder

--- airbnb-experience-clone/src/components/Card.js
+++ airbnb-experience-clone/src/components/Card.js
@@ -1,5 +1,18 @@
 import React from "react"
+import photoexperience from "../images/katie-zaferes.png"
+import star from "../images/star.png"
 
 export default function Card(){
-    return <h1>Card here</h1>
+    return <div className="experience-photos">
+        <div className="experience-listing">
+        <img className="experience-img" src={photoexperience} alt="Katie Zaferes in swimsuit/cap"></img>
+        <div className="review-div">
+        <img className="star-img" src={star} alt="star"></img>
+        <span className="review-details">5.0 (6) * USA</span>
+        </div>
+        {/* end review div */}
+        <p className="experience-heading">Life lessons with Katie Zaferes</p>
+        </div>
+
+    </div>
 }
