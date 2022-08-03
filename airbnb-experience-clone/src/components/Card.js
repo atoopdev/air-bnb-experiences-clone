0f8ac6535f43cdf9c5a33cb4a7ed054312c45e25
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-08-03 15:38:45 -0700

    more card css styling

--- airbnb-experience-clone/src/components/Card.js
+++ airbnb-experience-clone/src/components/Card.js
@@ -8,12 +8,17 @@ export default function Card(){
         <img className="experience-img" src={photoexperience} alt="Katie Zaferes in swimsuit/cap"></img>
         <div className="review-div">
         <img className="star-img" src={star} alt="star"></img>
-        <span className="review-details">5.0 (6) * USA</span>
+            <span>5.0</span>
+            <span className="grey"> (6) * </span>
+            <span className="grey">USA</span>
+            
         </div>
         {/* end review div */}
         <p className="experience-heading">Life lessons with Katie Zaferes</p>
         <p className="experience-price"><span className="bold">From $136</span> / person</p>
         </div>
+        {/* holds one photo */}
 
     </div>
+    // holds all photos
 }
